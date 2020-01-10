b119588d6c1908ee4aff7f2bc0b6d927ed8bcce8
Neil Bateman <Neil@Rivhas-MacBook-Pro.local>
2020-01-10 12:19:00 -0800

    add mapview library, api key for google maps

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        initialRegion={{
+          latitude: 45.5289487,
+          longitude: -122.6983163,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider={MapView.PROVIDER_GOOGLE}
+        style={styles.mapStyle}
+        />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 15,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+  }
 });
